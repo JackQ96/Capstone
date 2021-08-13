2706d4696554ae660faa7327e2b34d3dc9bee221
Jack Quinn <Jack@Jacks-MacBook-Pro.local>
2021-08-13 23:37:02 +0100

    pep8 changes

--- test_capstone.py
+++ test_capstone.py
@@ -12,9 +12,6 @@ DB_NAME = os.getenv('DB_NAME', 'capstone')
 DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5000')
 
 
-
-
-
 class CapstoneTestCase(unittest.TestCase):
     """This class represents the capstone test case"""
 
@@ -23,7 +20,7 @@ class CapstoneTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = DB_NAME
-        self.database_path = "postgresql+psycopg2://{}:{}@{}/{}".format(DB_USER,DB_PASSWORD,
+        self.database_path = "postgresql+psycopg2://{}:{}@{}/{}".format(DB_USER, DB_PASSWORD,
         DB_HOST, self.database_name)
         self.PRODUCER_JWT = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InhJVEtoa2NnSjNBNHU0SjBoMjBxVCJ9.eyJpc3MiOiJodHRwczovL2phY2txLmV1LmF1dGgwLmNvbS8iLCJzdWIiOiJhdXRoMHw2MTEyYzdlY2MxZmRjYjAwNzE5Yjk5NWYiLCJhdWQiOiJjYXBzdG9uZSIsImlhdCI6MTYyODg4ODI0MiwiZXhwIjoxNjI4ODk1NDQyLCJhenAiOiJOQjhOcDlkU2dOS29MUUU5ZDlzc21uajlhdDZGOEVmRyIsInNjb3BlIjoiIiwicGVybWlzc2lvbnMiOlsiZGVsZXRlOmFjdG9ycyIsImRlbGV0ZTptb3ZpZXMiLCJnZXQ6YWN0b3JzIiwiZ2V0Om1vdmllcyIsInBhdGNoOmFjdG9ycyIsInBhdGNoOm1vdmllcyJdfQ.Bm3TmnVaT17cWGEmbOMpvdiz34ROBkZsJ6biUOQStsGFqojXW3BMVW4SI5iLpeklTW4poYnSGuVbp5YTOASj7jqq1SqSHShM3_cl-MhCHuJLKolQmtw_rQF3Yh7SaLcWHW9sMZx-x-pfv8jXI6SEp-DzCdFGYA1_VlUDEPJIHAzAwvcGMEbv6Ng3Gc5RMrUtZer9V0v5XegW6k3RZXbKd8om4CIcz2Li8K1GhSRAzu0kZNK9CVDKh6JiMo0cunFuN2hHtthkbhbgxVuGl7adLXUzR0jeH4dWN7pNLVxMtifVjPWFhvsU2HjOa2SUwpZl41Fd3_pTlBh5WDriCblY2Q'
         self.CASTING_AGENT_JWT = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InhJVEtoa2NnSjNBNHU0SjBoMjBxVCJ9.eyJpc3MiOiJodHRwczovL2phY2txLmV1LmF1dGgwLmNvbS8iLCJzdWIiOiJhdXRoMHw2MTEyYzg0YTFjNWFlZjAwNmFlNDA5NmMiLCJhdWQiOiJjYXBzdG9uZSIsImlhdCI6MTYyODg4ODQ0NCwiZXhwIjoxNjI4ODk1NjQ0LCJhenAiOiJOQjhOcDlkU2dOS29MUUU5ZDlzc21uajlhdDZGOEVmRyIsInNjb3BlIjoiIiwicGVybWlzc2lvbnMiOlsiZ2V0OmFjdG9ycyIsImdldDptb3ZpZXMiXX0.Ico7aoYPubcQrgl787tDHwd83l7Jv7T10arMB7esv27VlkM5wF7IjDa0UVtPIuxf0Gpy8ajFs-pVFWsLmoMKu1m0EowQou1HAZPkO6zkNsLV8aT8KZMgLSa-sxL-fGtDB2KLrmK9U8QxLsThO_j2OkbWAWJyGMAmhdD25TqINpNjZv-iBxolqyUU5pWbt58U1rSbhhf8jgzAblEV-Ixc9nzxute1UKo6KLNwZsmhV8khgnYtTsiT3jG412f2eISgYoVpQyWNdcKr-7U2kClvHcl4hCkLAfOKAqw_2K9Vz3QkS_-sXrlA_X8UUW5o_joRkDLuNEJn2Jap6uDIAlAhqQ'
